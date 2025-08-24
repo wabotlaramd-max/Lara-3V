ee2ea8782f1fb8c47d91666d2a15344a2965a9d0
wabotlaramd-max <wabotlaramd@gmail.com>
2025-08-24 09:08:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,12 +5,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "vcwUWajJ#DJ9fP7Wl-vSXKViGuNJGYTTopE_GcQqffOGTKq_k-ow", // Add your session id
 PREFIX: process.env.PREFIX || ".",
-MODE: process.env.MODE || "public",  //public | private | inbox | group
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
-DEV: process.env.DEV || "94779062397",
-LANG: process.env.LANG || "EN", // EN | SI
+MODE: process.env.MODE || "private",  //public | private | inbox | group
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94741116425",
+DEV: process.env.DEV || "94741116425",
+LANG: process.env.LANG || "SI", // EN | SI
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
